949096477ba26cccd05424672285f5a637c12513
tkhsksk <tkhsksk0318@gmail.com>
2025-04-23 11:59:58 +0900

    admin
    adminのみセッション取得
    ユーザー取得が可能

--- handlers/session.go
+++ handlers/session.go
@@ -10,23 +10,23 @@ import (
 	"api/middlewares"
 )
 
-func GetSessions(c echo.Context) error {
+func GetUserSessions(c echo.Context) error {
 	// デフォルト 100 件 上限1000（DoS対策）
 	limit := middlewares.ParseLimitParam(c, 100, 1000)
 
-	var sessions []models.Session
+	var sessions []models.UserSession
 	if err := db.DB.Limit(limit).Find(&sessions).Error; err != nil {
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "セッション取得に失敗しました"})
 	}
 
 	// 必要な情報だけをマッピング
-	var response []models.SessionResponse
+	var response []models.UserSessionResponse
 	for _, u := range sessions {
-		response = append(response, models.SessionResponse{
+		response = append(response, models.UserSessionResponse{
 			ID:        u.ID,
 			UserID:    u.UserID,
-			ExpiresAt: u.ExpiresAt,
 			CreatedAt: u.CreatedAt,
+			ExpiresAt: u.ExpiresAt,
 		})
 	}
 
@@ -38,18 +38,18 @@ func GetSessionsByUserID(c echo.Context) error {
 	limit := middlewares.ParseLimitParam(c, 100, 1000)
 	// urlからid取得
 	id := c.Param("id")
-	var sessions []models.Session
+	var sessions []models.UserSession
 	if err := db.DB.Limit(limit).Where("user_id = ?", id).Find(&sessions).Error; err != nil {
 		return c.JSON(http.StatusNotFound, echo.Map{"error": "セッションが見つかりません"})
 	}
 
 	// 必要な情報だけをマッピング
-	var response []models.SessionUserResponse
+	var response []models.UserSessionResponseByUserID
 	for _, u := range sessions {
-		response = append(response, models.SessionUserResponse{
+		response = append(response, models.UserSessionResponseByUserID{
 			ID:        u.ID,
-			ExpiresAt: u.ExpiresAt,
 			CreatedAt: u.CreatedAt,
+			ExpiresAt: u.ExpiresAt,
 		})
 	}
 
